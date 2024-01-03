eb1a77b21b5721b4f8bb4167c3639030cf584f33
jlai403 <joey@jlai.ca>
2024-01-03 00:31:31 -0700

    updated home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="splash-screen">
+    <div class="name">Crystal</div>
+    <div class="and">and</div>
+    <div class="name">Joey</div>
   </main>
 </template>
+
+<style scoped>
+.splash-screen {
+  text-align: center;
+}
+
+.name {
+  font-family: 'Buda', serif;
+  text-transform: uppercase;
+  font-size: 5rem;
+  line-height: 1.5;
+  letter-spacing: 1rem;
+  color: var(--vt-c-white);
+}
+
+.and {
+  color: var(--vt-c-white);
+  font-family: 'Allura', cursive;
+  z-index: 1;
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-top: -2rem;
+  font-size: 2rem;
+}
+</style>
