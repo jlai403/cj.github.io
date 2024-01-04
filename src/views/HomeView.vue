415a8ca70a63ef39dbf2590db5d29a52aa8d1f75
jlai403 <joey@jlai.ca>
2024-01-03 21:23:02 -0700

    updated home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,35 +2,48 @@
 </script>
 
 <template>
-  <main class="splash-screen">
+  <main class="hero-image">
     <div class="name">Crystal</div>
-    <div class="and">and</div>
+    <div class="and">────&emsp;and&emsp;────</div>
     <div class="name">Joey</div>
+    <p class="sub-text">Since MMXXII.I.XXII</p>
   </main>
 </template>
 
 <style scoped>
-.splash-screen {
+.hero-image {
+  border: 1px solid #ffffff80;
+  background-color: rgba(255, 255, 255, 0.1);
+  padding: 2rem 5rem;
   text-align: center;
 }
 
 .name {
-  font-family: 'Buda', serif;
-  text-transform: uppercase;
-  font-size: 5rem;
-  line-height: 1.5;
-  letter-spacing: 1rem;
   color: var(--vt-c-white);
+  font-family: 'Quattrocento', serif;
+  font-size: 5rem;
+  letter-spacing: 1rem;
+  line-height: 1.5;
+  text-transform: uppercase;
 }
 
 .and {
+  margin-top: -2rem;
   color: var(--vt-c-white);
   font-family: 'Allura', cursive;
-  z-index: 1;
+  font-size: 2rem;
   position: absolute;
+  z-index: 1;
   left: 0;
   right: 0;
-  margin-top: -2rem;
-  font-size: 2rem;
+}
+
+.sub-text {
+  color: var(--vt-c-black);
+  font-family: 'Quattrocento', cursive;
+  font-size: 1rem;
+  font-weight: bold;
+  text-transform: uppercase;
+
 }
 </style>
